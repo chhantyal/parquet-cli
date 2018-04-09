a39aff2fe77e995a13fe54c3c08cd5dbc6baae96
Nar Kumar Chhantyal <chtn@viessmann.com>
2018-04-09 23:23:33 +0200

    add setup file to upload to PyPI and make it installable

---
+++ setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='parquet-cli',
+    version='1.0',
+    description='Command line (CLI) tool to inspect Apache Parquet files on the go',
+    author='Nar Kumar Chhantyal',
+    author_email='nkchhantyal@gmail.com',
+    url='https://github.com/chhantyal/parquet-cli',
+    license='BSD 3-Clause License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    packages=find_packages(),
+    platforms='any',
+    install_requires=[
+        'pandas',
+        'pyarrow'
+    ],
+    entry_points={
+        'console_scripts': [
+            'parq = parq.main:main',
+        ]
+    },
+    package_data={'data': ['*.parquet']},
+    include_package_data=True,
+)
